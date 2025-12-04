e6d43f97ced2441a9a9b3ed3e33f4af8eaa1dac4
vos0w01 <w.draaisma-vos@belastingdienst.nl>
2025-12-04 13:08:41 +0100

    default settings weer

--- variables.tf
+++ variables.tf
@@ -26,7 +26,7 @@ variable "instance_name_prefix" {
 
 variable "instance_type" {
   description = "Specifies the AWS instance type."
-  default     = "t2.micro"
+  default     = "t3.micro"
 }
 
 variable "security_group_name" {
